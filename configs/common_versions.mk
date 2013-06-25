b89166fac96beda57dfb9adb304a9b5c9c254fc5
Orca Project <shamukillaz@orcaproject.co>
2013-06-25 18:33:06 -0400

    Orca Update

--- configs/common_versions.mk
+++ configs/common_versions.mk
@@ -16,7 +16,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.goo.developerid=drewgaren \
     ro.goo.rom=Orca_Nightlies \
     ro.goo.version=$(DATE) \
-    ro.orca.version=Orca-2.2.0-$(TARGET_PRODUCT)-Nightly-$(DATE)
+    ro.orca.version=Orca-2.2.1-$(TARGET_PRODUCT)-Nightly-$(DATE)
 endif
 
 
